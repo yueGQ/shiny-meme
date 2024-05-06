7ccc0f45fc4f449bf83f3197f97401ec6230efeb
yue <952012316@qq.com>
2024-05-06 13:43:24 +0800

    a第二次提交

--- src/components/setup.vue
+++ src/components/setup.vue
@@ -2,6 +2,11 @@
   <h1>哈哈哈哈哈哈哈</h1>
   <h1>哈哈哈哈哈哈哈</h1>
   <h1>哈哈哈哈哈哈哈</h1>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
