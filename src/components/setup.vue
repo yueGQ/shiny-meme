b068139332d4fb0cec0bad9b479661259a126491
yue <952012316@qq.com>
2024-05-06 14:30:48 +0800

    提交

--- src/components/setup.vue
+++ src/components/setup.vue
@@ -6,6 +6,10 @@
   </ul>
   <p>她慢慢不在是一个男孩</p>
   <p>她慢慢不在是一个男孩</p>
+  <h1>你吓跑四物品</h1>
+  <h1>你吓跑四物品</h1>
+  <h1>你吓跑四物品</h1>
+  <h1>你吓跑四物品</h1>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
